ca188260f69687dce8f9d52086443d42498d4639
hariram <harirams@am.amrita.edu>
2014-12-03 16:22:44 +0530

    Initial commit

---
+++ JSProj/js/namespace.js
@@ -0,0 +1,29 @@
+/**************************************
+* Creates namespaces in javascript 
+* 
+***************************************
+* Version: 0.1
+* Created: 3-Dec-2014
+* Comment: Initial version
+* Authors: Hariram S
+*
+* Copyright (c) 2014 Hariram S
+* Licensed under the GNU GPL v2.0 license.
+* https://github.com/hariram1981/js_musings/blob/master/LICENSE 
+* 
+**************************************/
+
+var namespace = function() {
+}
+
+namespace.create = function(names) {
+	//If array then create function for all
+	//Else create only one
+	//if(Array.isArray()) {
+		for(a in names) {
+			window[names[a]] = function() {};
+		}
+	//} else {
+	//	window[names] = function(){};
+	//}
+}
