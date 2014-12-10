a6f29e44d1beb2913136fc917023ed2988492368
hariram <harirams@am.amrita.edu>
2014-12-10 10:12:47 +0530

    Adding treeset in javascript

---
+++ JSProj/js/TreeSet.js
@@ -0,0 +1,79 @@
+/**************************************
+* TreeSet - creates a set of values (list
+* of objects without duplicates) that
+* is ordered in ascending.
+* 
+* Functions supported are:
+*   - count - returns count of objects in set
+*   - add - adds the object to the set
+*   - exists - returns true if object exists in the set 
+*   - listValues - returns an array of all values in the list
+***************************************
+* Version: 0.1
+* Created: 10-Dec-2014
+* Comment: 10-Dec-2014 - initial version
+* Authors: Hariram S
+* 
+* Copyright (c) 2014 Hariram S
+* Licensed under the GNU GPL v2.0 license.
+* https://github.com/hariram1981/js_musings/blob/master/LICENSE
+* 
+**************************************/
+
+function TreeSet() {
+  var _data = [];
+  var _size = 0;
+  this.findIndex = function(val) {
+    return _data.indexOf(val);
+  },
+  Object.defineProperty(this, "data", {
+	get: function() {
+	  return _data;
+	},
+	set: function(newValue) {
+	  _data = newValue;
+	}
+  }),
+  Object.defineProperty(this, "size", {
+	get: function() {
+	  return _size;
+	},
+	set: function(newValue) {
+	  _size = newValue;
+	}
+  });
+}
+
+TreeSet.prototype = {
+  version: '0.1',
+  constructor: Set,
+  add: function(value) {
+	 if(!value) {
+       console.warn("no value provided");
+	   return;
+	 } 
+     //Check if key exists and if so, then update the map
+	 var index = this.findIndex(value);
+     if(index == -1) {
+	   this.data[this.size] = value;
+       this.size = this.size + 1;
+       this.data.sort();
+	 } else {
+	   console.warn(value + " already exists");
+	 }
+  }, 
+  exists: function(value) {
+    var index = this.findIndex(value);
+    if(index == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  count: function() {
+    return (this.size);
+  },
+  listValues: function() {
+    return this.data;
+  }
+}
